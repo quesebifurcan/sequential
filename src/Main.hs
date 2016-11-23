08f6e5b1365c77257904d81ccd8a2d85fa3ceec2
Fredrik Wallberg <fredrik.wallberg@gmail.com>
2016-11-23 23:07:11 +0100

    data: add Note and Chord types

--- src/Main.hs
+++ src/Main.hs
@@ -5,11 +5,32 @@ module Main where
 
 import Protolude
 
+type Pitch = Ratio Int
+type Duration = Ratio Int
+
 data Note = Note {
-  pitch :: Int
-  , duration :: Int
+  pitch      :: Pitch,
+  duration   :: Duration,
+  instrument :: Text
   } deriving Show
+
+data Chord = Chord {
+  deltaDuration :: Duration,
+  notes         :: [Note]
+  } deriving Show
+
+makeChord :: Pitch -> Duration -> Duration -> Text -> Chord
+makeChord pitch duration deltaDuration instrument = Chord {
+  deltaDuration = deltaDuration,
+  notes = [
+      Note {
+          pitch = pitch,
+          duration = duration,
+          instrument = instrument
+          }
+      ]
+  }
 
 main :: IO ()
 main = do
-  print (Note { pitch = 0, duration = 1 })
+  print $ makeChord (7 % 4) 1 2 "Clarinet"
