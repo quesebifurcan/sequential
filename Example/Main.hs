7aca431e6cdddd062718f61a577374d62dd594ef
Fredrik Wallberg <fredrik.wallberg@gmail.com>
2016-12-22 21:49:00 +0100

    move score-specific functions to Example

--- Example/Main.hs
+++ Example/Main.hs
@@ -22,6 +22,8 @@ import Sequential
 
 toFloat x = fromRational x :: Float
 
+printQuoted = PP.enclose PP.squote PP.squote . PP.string
+
 printSound :: Int -> Sound -> PP.Doc
 printSound nodeId sound =
   PP.list [PP.float start', PP.list expr]
@@ -33,19 +35,96 @@ printSound nodeId sound =
         frequency                                  = toFloat $ baseFreq * (octave % 1) * ratio
         (Velocity velocity')                       = (velocity sound)
         expr = [
-          PP.string "'s_new'"
-          , PP.string "'sine'"
+          printQuoted "s_new"
+          , printQuoted "sine"
           , PP.int nodeId
           , PP.int 0
           , PP.int 0
-          , PP.string "'frequency'"
+          , printQuoted "frequency"
           , PP.float frequency
-          , PP.string "'duration'"
+          , printQuoted "duration"
           , PP.float duration
           ]
 
 printSounds :: [Sound] -> PP.SimpleDoc
 printSounds sounds = PP.renderOneLine . PP.list $ zipWith printSound [1000..] sounds
+
+pitches = [
+  soundDefault {
+      pitch = (Pitch (PitchRatio (1%1)) (Octave 4)),
+      start = TimePoint 0,
+      minDuration = Duration 1,
+      deltaDuration = Duration 1
+      },
+  soundDefault {
+      pitch = (Pitch (PitchRatio (5%4)) (Octave 4)),
+      start = TimePoint 0,
+      minDuration = Duration 1,
+      deltaDuration = Duration (2)
+      },
+  soundDefault {
+      pitch = (Pitch (PitchRatio (3%2)) (Octave 4)),
+      start = TimePoint 2,
+      minDuration = Duration 1,
+      deltaDuration = Duration (3)
+      },
+  soundDefault {
+      pitch = (Pitch (PitchRatio (7%4)) (Octave 4)),
+      start = TimePoint 0,
+      minDuration = Duration 1,
+      deltaDuration = Duration (4)
+      }
+  ]
+
+soundDefault = Sound {
+  pitch           = Pitch (PitchRatio (1 % 1)) (Octave 0)
+  , velocity      = Velocity 127
+  , start         = TimePoint 0
+  , stop          = TimePoint 1
+  , minDuration   = Duration 1
+  , maxDuration   = Duration 2
+  , deltaDuration = Duration 1
+  , constraint    = MomentConstraint { dissonanceLimit = Set.empty
+                                     , maxCount = 4 }
+  , envelope      = Sustained
+  , instrument    = Instrument { range = (48, 60) }
+  }
+
+slope :: (Ord t, Floating t, Enum t) => t -> t -> [t]
+slope exponent count =
+  let range' = map (\x -> x ** exponent) [0..count]
+      max' = maximum range'
+      range'' = map (/ max') range'
+  in (List.init . drop 1) range''
+
+a = Protolude.map (\x -> (x, 'c')) $ slope 1.9 5
+b = Protolude.map (\x -> (x, 'd')) $ slope 1.7 8
+c = Protolude.map (\x -> (x, 'e')) $ slope 1.5 13
+d = Protolude.map (\x -> (x, '.')) $ slope 1.3 87
+e = map snd $ sort $ a ++ b ++ c ++ d
+
+melos :: Int -> Instruments -> V.Validation [SoundErrors] [Sound]
+melos n instrumentMap =
+  let pitchRatios = [1 % 1, 5 % 4, 3 % 2, 7 % 4]
+      octaves = [1, 1, 2, 8, 1]
+      velocities = [0, 1, 2, 8, 1]
+      instruments = ["instrument_1"]
+  in
+    sequenceA $
+    take n $
+    getZipList $
+    (mkSound instrumentMap) <$>
+    ZipList (cycle pitchRatios) <*>
+    ZipList (cycle octaves) <*>
+    ZipList (cycle velocities) <*>
+    ZipList (cycle instruments)
+
+testDListAppend :: MonadIO m => m ()
+testDListAppend =
+  mapM_ print
+  $ fmap (\x -> (start x, instrument x, (ratio . pitch) x))
+  $ _result
+  $ run' (take 400000 (cycle pitches))
 
 main :: IO ()
 main = do
--- src/Sequential.hs
+++ src/Sequential.hs
@@ -165,20 +165,6 @@ mkSound instrumentMap pitchRatio octave velocity instrumentName =
     pure MomentConstraint { dissonanceLimit = Set.empty, maxCount = 4 } <*>
     instrument'
 
-soundDefault = Sound {
-  pitch           = Pitch (PitchRatio (1 % 1)) (Octave 0)
-  , velocity      = Velocity 127
-  , start         = TimePoint 0
-  , stop          = TimePoint 1
-  , minDuration   = Duration 1
-  , maxDuration   = Duration 2
-  , deltaDuration = Duration 1
-  , constraint    = MomentConstraint { dissonanceLimit = Set.empty
-                                     , maxCount = 4 }
-  , envelope      = Sustained
-  , instrument    = Instrument { range = (48, 60) }
-  }
-
 data Moment = Moment {
   _now :: TimePoint
   , _active :: Set Sound
@@ -348,69 +334,6 @@ run' :: Foldable t => t Sound -> Moment
 run' xs =
   fadeOut result
   where result = foldl' resolveMoment momentDefault xs
-
-pitches = [
-  soundDefault {
-      pitch = (Pitch (PitchRatio (1%1)) (Octave 4)),
-      start = TimePoint 0,
-      minDuration = Duration 1,
-      deltaDuration = Duration 1
-      },
-  soundDefault {
-      pitch = (Pitch (PitchRatio (5%4)) (Octave 4)),
-      start = TimePoint 0,
-      minDuration = Duration 1,
-      deltaDuration = Duration (2)
-      },
-  soundDefault {
-      pitch = (Pitch (PitchRatio (3%2)) (Octave 4)),
-      start = TimePoint 2,
-      minDuration = Duration 1,
-      deltaDuration = Duration (3)
-      },
-  soundDefault {
-      pitch = (Pitch (PitchRatio (7%4)) (Octave 4)),
-      start = TimePoint 0,
-      minDuration = Duration 1,
-      deltaDuration = Duration (4)
-      }
-  ]
-
-slope :: (Ord t, Floating t, Enum t) => t -> t -> [t]
-slope exponent count =
-  let range' = map (\x -> x ** exponent) [0..count]
-      max' = maximum range'
-      range'' = map (/ max') range'
-  in (List.init . drop 1) range''
-
-a = Protolude.map (\x -> (x, 'c')) $ slope 1.9 5
-b = Protolude.map (\x -> (x, 'd')) $ slope 1.7 8
-c = Protolude.map (\x -> (x, 'e')) $ slope 1.5 13
-d = Protolude.map (\x -> (x, '.')) $ slope 1.3 87
-e = map snd $ sort $ a ++ b ++ c ++ d
-
-melos :: Int -> Instruments -> V.Validation [SoundErrors] [Sound]
-melos n instrumentMap =
-  let pitchRatios = [1 % 1, 5 % 4, 3 % 2, 7 % 4]
-      octaves = [1, 1, 2, 8, 1]
-      velocities = [0, 1, 2, 8, 1]
-      instruments = ["instrument_1"]
-  in
-    sequenceA $
-    take n $
-    getZipList $
-    (mkSound instrumentMap) <$>
-    ZipList (cycle pitchRatios) <*>
-    ZipList (cycle octaves) <*>
-    ZipList (cycle velocities) <*>
-    ZipList (cycle instruments)
-
-testDListAppend :: MonadIO m => m ()
-testDListAppend =
-  mapM_ print
-  $ fmap (\x -> (start x, instrument x, (ratio . pitch) x))
-  $ _result
-  $ run' (take 400000 (cycle pitches))
 
 -- TODO:
 -- 1. change Instrument type to use (midiNote, baseFreq, pitchRatio, Octave)
